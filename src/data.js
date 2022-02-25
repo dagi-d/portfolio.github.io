83401f3c9ddbaf61e60ca019f4ec7849e09a834b
Dagmawi Bekele <dagemawibekele@gmail.com>
2022-02-25 04:42:40 -0800

    deploy my portfolio on github

---
+++ src/data.js
@@ -0,0 +1,49 @@
+
+export const projects = [
+    {
+      title: "Supermarket Online Shopping",
+      subtitle: "PHP HTML CSS MYSQL",
+      description:
+        "The project is a web based system intended for online retailers. The customer can order any product stored in the supermarket and request delivery. I have participate in this project as team leader and coding ****",
+      image: "./project1.jpg",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "Online Electronic Shopping",
+      subtitle: "MERN STACK",
+      description:
+        "The project is a web based system intended for online retailers. The customer can order any electronic product posted and system admin can insert and manage any product. I have built this project independently",
+      image: "./project2.jpeg",
+      link: "https://reedbarger.com",
+    }
+  ];
+
+  
+export const skills = [
+    {
+      title: "React.js",
+    },
+    {
+        title: "node.js",
+      },
+      {
+        title: "mongoDB",
+      },
+      {
+        title: "MySQL",
+      },
+      {
+          title: "Django Python",
+        },
+
+        {
+          title: "CSS",
+        },
+        {
+            title: "HTML",
+          }
+          ,
+        {
+            title: "PHP",
+        }  
+  ];
